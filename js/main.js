75be745813d20e024e0e84f2db2b83287051f7c8
Salman Farshe <salmanfarshe39@gmail.com>
2021-01-23 10:55:36 +0600

    modified

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 let myli = document.querySelectorAll("li");
 
 for(let i = 0; i < myli.length; i++){
+    // mouse over Effect
     myli[i].addEventListener("mouseover", function(){
         //this.style.color = "#00ff00";
         this.classList.add("selected");
     });
+
+    // mouse out Effect
     myli[i].addEventListener("mouseout", function(){
         //this.style.color = "black";
         this.classList.remove("selected");
     });
+
+    // mouse click Effect
     myli[i].addEventListener("click", function(){
         this.classList.toggle("done");
     });
